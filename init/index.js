40c76452e3576a19200a2aeb1069b15ce6faa37c
krishna3336u <krishnasinghbirana93@gmail.com>
2026-01-30 18:53:27 +0530

    first commit

---
+++ init/index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+    return initDB();
+  })
+  .catch((err) => {
+    console.error("Error connecting to DB:", err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  try {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+  } catch (err) {
+    console.error("Error initializing data:", err);
+  } finally {
+    mongoose.connection.close(); // Close connection
+  }
+};
+initDB();
