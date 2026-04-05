af4bfe709568a47e379715b146f64d1d6191a356
Krishna Singh <krishnasinghbirana93@gmail.com>
2026-04-05 16:48:27 +0530

    fix homepage route

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://krishnasingh:Krishna%4078%2378@cluster0.rrzxquz.mongodb.net/wanderlust"
+
 
 main()
   .then(() => {
